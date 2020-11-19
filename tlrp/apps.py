e131ec3f4bdb81f6d3ac28b85e8106c6a43c6ee9
Quang Dang <quangdangfit@gmail.com>
2020-11-19 11:16:50 +0700

    rename directory

--- tlrp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class TlrpConfig(AppConfig):
-    name = 'tlrp'
--- tlrp/wsgi.py
+++ tlrp/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for TLRP_project project.
+WSGI config for tlrp project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TLRP_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tlrp.settings')
 
 application = get_wsgi_application()
