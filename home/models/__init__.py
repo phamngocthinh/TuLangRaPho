ffb04681c71fc62d070237a1284e57e08a5d0281
Quang Dang <quangdangfit@gmail.com>
2020-11-19 14:35:43 +0700

    update models

---
+++ home/models/__init__.py
@@ -0,0 +1,7 @@
+from .category import *
+from .discount import *
+from .product import *
+from .product_translate import *
+from .product_type import *
+from .user import *
+from .role import *
--- home/models/user.py
+++ home/models/user.py
@@ -1,10 +1,12 @@
 from django.db import models
 
+from home.models.role import Role
+
 
 class User(models.Model):
-    id = models.SmallAutoField(db_column='id', primary_key=True)
+    id = models.AutoField(db_column='id', primary_key=True)
     name = models.CharField(db_column='name', max_length=100, blank=True, null=True)
-    user_role_id = models.SmallIntegerField(db_column='user_role_id', blank=True, null=True)
+    role_id = models.ForeignKey(to=Role, on_delete=models.CASCADE, db_column='role_id', blank=True)
     user_name = models.CharField(db_column='user_name', max_length=50, blank=True, null=True)
     password = models.CharField(db_column='password', max_length=100, blank=True, null=True)
     enable = models.IntegerField(db_column='enable', blank=True, null=True)
