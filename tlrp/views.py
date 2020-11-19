52f97acea38eea6785fd4df7830fd136d2dbcdf3
thinhpn <pnthinh.fit.hcmus@gmail.com>
2020-11-19 10:51:35 +0700

    init project

---
+++ tlrp/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.views.generic import TemplateView
+
+
+class HomeView(TemplateView):
+    template_name = 'index.html'
+
+    def get_header(seft, request):
+        return render(request, seft.template_name)
