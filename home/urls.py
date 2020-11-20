156bf316e803132318752b537907f9fac3b0cc00
Quang Dang <quangdangfit@gmail.com>
2020-11-20 15:16:38 +0700

    add about page

--- home/urls.py
+++ home/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from home.views import home
-from home.views import blog
+from home.views import home, blog, about, contact
 
 urlpatterns = [
     path('', home.HomeView.as_view(), name='home'),
     path('blogs/', blog.BlogView.as_view(), name='blog'),
+    path('about/', about.AboutView.as_view(), name='about'),
+    path('contact/', contact.ContactView.as_view(), name='contact'),
 ]
