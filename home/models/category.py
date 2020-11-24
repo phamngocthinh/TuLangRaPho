a19a2ab14846673c20c6035efabf6c719b2b635c
Đặng Phước Quang <quangdangfit@gmail.com>
2020-11-24 13:56:26 +0700

    update model category

--- home/models/category.py
+++ home/models/category.py
@@ -2,11 +2,5 @@ from django.db import models
 
 
 class Category(models.Model):
-    id = models.AutoField(db_column='id', primary_key=True)
-    name = models.CharField(db_column='name', max_length=250, blank=True, null=False)
-
     class Meta:
         db_table = 'categories'
-
-    def __str__(self):
-        return self.name
