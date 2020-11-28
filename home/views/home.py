454fe8da9fc5e980fdc75821a5202b4ad6abec86
Đặng Phước Quang <quangdangfit@gmail.com>
2020-11-28 19:34:19 +0700

    show categories

--- home/views/home.py
+++ home/views/home.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.views.generic import TemplateView
+from home.models import CategoryTranslate
 
 
 class HomeView(TemplateView):
@@ -9,3 +10,10 @@ class HomeView(TemplateView):
 
     def get_header(seft, request):
         return render(request, seft.template_name)
+
+    def get_context_data(self, **kwargs):
+        categories = CategoryTranslate.objects.all()
+        context = {
+            'categories': categories
+        }
+        return context
