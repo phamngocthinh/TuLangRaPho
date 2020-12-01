691f764c8bc7837670bcecc8269d523b1f31391b
ThinhPN <pnthinh.fit.hcmus@gmail.com>
2020-12-01 07:25:55 +0700

    binding data to blog page

--- home/urls.py
+++ home/urls.py
@@ -6,8 +6,8 @@ from home.views import home, blog, about, contact, category
 
 urlpatterns = [
     path('', home.HomeView.as_view(), name='home'),
-    path('category/<int:id>', category.CategoryView.as_view(), name='category'),
-    path('blogs/', blog.BlogView.as_view(), name='blog'),
-    path('about/', about.AboutView.as_view(), name='about'),
-    path('contact/', contact.ContactView.as_view(), name='contact'),
+    path(r'category/<int:id>', category.CategoryView.as_view(), name='category'),
+    path(r'blogs/', blog.BlogView.as_view(), name='blog'),
+    path(r'about/', about.AboutView.as_view(), name='about'),
+    path(r'contact/', contact.ContactView.as_view(), name='contact'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- home/views/blog.py
+++ home/views/blog.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.views.generic import TemplateView
+from home.models import Blog, BlogTranslate
 
 
 class BlogView(TemplateView):
@@ -9,3 +10,16 @@ class BlogView(TemplateView):
 
     def get_header(seft, request):
         return render(request, seft.template_name)
+
+    def get_context_data(self, **kwargs):
+        # blogs = Blog.objects.all()
+        # blog_translates = BlogTranslate.objects.all()
+        # # blog_translates = BlogTranslate.objects.filter(blog_id=blogs.values('id'))
+        # context = {
+        #     'blog_translates': blog_translates.filter(lang_code='vn')
+        # }
+        queryset = BlogTranslate.objects.select_related('blog_id')
+        blog_translates = []
+        for blog_translate in queryset:
+            blog_translates.append({'title': blog_translate.title})
+        return blog_translates
