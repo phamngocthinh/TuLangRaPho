ffe1c9b4eafea305624a0bb6190e852b2dfe89d9
thinhpn <pnthinh.fit.hcmus@gmail.com>
2020-12-30 17:43:38 +0700

    edit product image

--- home/views/home.py
+++ home/views/home.py
@@ -13,7 +13,7 @@ class HomeView(TemplateView):
 
     def get_context_data(self, **kwargs):
         categories = CategoryTranslate.objects.all()
-        products = ProductTranslate.objects.all()
+        products = ProductTranslate.objects.all().select_related('product_id')
         product_types = ProductTypeTranslate.objects.all()
         context = {
             'categories': categories.filter(lang_code='vn'),
