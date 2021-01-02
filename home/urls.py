aca74cf8a516f1e00c47b223154c23b25fd6b011
ThinhPN <pnthinh.fit.hcmus@gmail.com>
2021-01-02 13:28:09 +0700

    add product type page

--- home/urls.py
+++ home/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from home.views import home, blog, about, contact, category, product_detail
+from home.views import home, blog, about, contact, category, product_detail, type
 
 urlpatterns = [
     path('', home.HomeView.as_view(), name='home'),
     path(r'category/<int:id>', category.CategoryView.as_view(), name='category'),
+    path(r'type/<int:id>', type.TypeView.as_view(), name='type'),
     path(r'blogs/', blog.BlogView.as_view(), name='blog'),
     path(r'about/', about.AboutView.as_view(), name='about'),
     path(r'contact/', contact.ContactView.as_view(), name='contact'),
--- home/views/type.py
+++ home/views/type.py
@@ -2,30 +2,30 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.views.generic import TemplateView
-from home.models import CategoryTranslate, Category, ProductTranslate, Product, ProductTypeTranslate
+from home.models import ProductTypeTranslate, Category, ProductTranslate, Product, CategoryTranslate
 
 
-class CategoryView(TemplateView):
-    template_name = 'category/index.html'
+class TypeView(TemplateView):
+    template_name = 'type/index.html'
 
     def get_header(seft, request):
         return render(request, seft.template_name)
 
     def get_context_data(self, **kwargs):
-        # get category info with **kwargs
-        categories = CategoryTranslate.objects.all().filter(lang_code='vn', category_id=kwargs.get('id'))
-        # get all product of categories
-        products = Product.objects.all().filter(category_id=kwargs.get('id'))
-        # get all product of categories by language
+        # get type info with **kwargs
+        types = ProductTypeTranslate.objects.all().filter(lang_code='vn', type_id=kwargs.get('id'))
+        # get all product of type
+        products = Product.objects.all().filter(product_type_id=kwargs.get('id'))
+        # get all product of types by language
         products_translate = ProductTranslate.objects.all().filter(lang_code='vn',product_id__in=products)
-        # get all product types
-        product_types = ProductTypeTranslate.objects.all().filter(lang_code='vn')
+        # get all product categories
+        categories = CategoryTranslate.objects.all().filter(lang_code='vn')
         # get all product special
         special_products_translate = ProductTranslate.objects.all().filter(lang_code='vn').select_related('product_id')
         context = {
-            'category': categories[0],
+            'type': types[0],
             'products': products_translate,
-            'product_types': product_types,
+            'categories': categories,
             'special_products': special_products_translate.order_by('product_id__rank')[:5]
         }
         return context
